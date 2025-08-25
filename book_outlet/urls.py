b0b63eab20d22c636e1fe50c56035705a83ac65b
Gordon Situ <gsitu322@gmail.com>
2025-08-25 12:56:36 -0700

    Using a slug for each book

--- book_outlet/urls.py
+++ book_outlet/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name="index"),
 
-    path('book/<int:id>', views.book_detail, name='book_detail'),
+    path("book/<int:id>", views.book_detail, name="book_detail"),
+
+    path("book/<slug:slug>", views.book_detail_slug, name="book_detail_slug"),
 ]
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -8,7 +8,13 @@ def index(request):
 
 def book_detail(request, id):
     book = get_object_or_404(Book, pk=id)
+    return render_book(request, book)
 
+def book_detail_slug(request, slug):
+    book = get_object_or_404(Book, slug=slug)
+    return render_book(request, book)
+
+def render_book(request, book):
     return render(request, 'book_outlet/book_detail.html', {
         "title" : book.title,
         "author" : book.author,
