8a80fc34b52e5909197a5339f1e2999979de1e85
Gordon Situ <gsitu322@gmail.com>
2025-08-25 13:02:22 -0700

    Using aggregation and order

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Book
+from django.db.models import Avg
 
 # Create your views here.
 def index(request):
-    books = Book.objects.all()
-    return render(request, 'book_outlet/index.html', {'books': books})
+    books = Book.objects.all().order_by('-rating')
+    return render(request, "book_outlet/index.html", {
+        "books": books,
+        "total_number_of_books": books.count(),
+        "avg_rating": books.aggregate(Avg("rating"))
+    })
 
 def book_detail(request, id):
     book = get_object_or_404(Book, pk=id)
