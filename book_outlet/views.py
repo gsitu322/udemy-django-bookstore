a339624736bd7ca54922c02dd05cb49f5fa43123
Gordon Situ <gsitu322@gmail.com>
2025-08-25 12:36:52 -0700

    Retrieving data and showing book details

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Book
 
 # Create your views here.
 def index(request):
-    return render(request, 'book_outlet/index.html')
+    books = Book.objects.all()
+    return render(request, 'book_outlet/index.html', {'books': books})
+
+def book_detail(request, id):
+    book = get_object_or_404(Book, pk=id)
+
+    return render(request, 'book_outlet/book_detail.html', {
+        "title" : book.title,
+        "author" : book.author,
+        "rating": book.rating,
+        "is_bestselling": book.is_bestselling,
+    })
